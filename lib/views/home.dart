317ee612e05779d94ef32f7d38507f6f9809f8e8
nifen <nife.niroopn@gmail.com>
2023-11-06 22:35:11 +0530

    3 : Created a News Widget

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:namma_samachara/views/widgets/news_widget.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: PageView.builder(
+        controller: PageController(initialPage: 0),
+        scrollDirection: Axis.vertical,
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return NewsWidget(
+            imgUrl: 'https://picsum.photos/250?image=9',
+            title: "5G in India",
+            description:
+                "5G is great on paper. But in real-world use, it has been synonymous with call drops, constant shuffle between 4G and 5G, and network vacuums. It has basically been a disaster and in this piece, we dig deeper into the poor experience.",
+            newsUrl:
+                "https://www.indiatoday.in/technology/talking-points/story/5g-is-available-in-india-in-almost-every-city-and-yet-so-far-its-rollout-has-been-a-disaster-for-users-2454636-2023-10-28",
+          );
+        },
+      ),
+    );
   }
 }
---
+++ lib/views/widgets/news_widget.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class NewsWidget extends StatelessWidget {
+  String imgUrl;
+  String title;
+  String description;
+  String newsUrl;
+
+  NewsWidget(
+      {required this.imgUrl,
+      required this.title,
+      required this.description,
+      required this.newsUrl,
+      super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Image.network(
+            imgUrl,
+            height: 400,
+            width: MediaQuery.of(context).size.width,
+            fit: BoxFit.cover,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 20),
+                Text(
+                  title,
+                  style: const TextStyle(
+                      fontSize: 30, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  description,
+                  style: const TextStyle(fontSize: 16),
+                ),
+              ],
+            ),
+          ),
+          const Spacer(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text("Read More"),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 5),
+        ],
+      ),
+    );
+  }
+}
